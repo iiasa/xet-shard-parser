85cce42dd884c5b71cc7114898cc703f674fbbdf
S Rupesh <wrufesh@gmail.com>
2026-04-03 10:37:04 +0200

    add consolidation helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
-use std::io::Cursor;
+use std::io::{Cursor, Read, Seek};
 use std::collections::HashSet;
 
 use mdb_shard::metadata_shard::streaming_shard::MDBMinimalShard;
+use mdb_shard::metadata_shard::set_operations::shard_set_union;
+use mdb_shard::metadata_shard::{MDBShardInfo, MDBShardFileHeader, MDBShardFileFooter};
 use mdb_shard::merklehash::MerkleHash;
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyDict, PyList};
+use std::mem::size_of;
+use std::mem::swap;
 
 #[pyfunction]
 #[allow(unsafe_op_in_unsafe_fn)]
@@ -87,8 +91,73 @@ pub fn extract_shard_metadata(
     Ok(root_dict.into_py(py))
 }
 
+#[pyfunction]
+#[allow(unsafe_op_in_unsafe_fn)]
+pub fn merge_shards(
+    py: Python<'_>,
+    shard_list: Vec<Vec<u8>>,
+    target_max_size: u64,
+) -> PyResult<Py<PyList>> {
+    if shard_list.is_empty() {
+        return Ok(PyList::empty(py).into_py(py));
+    }
+
+    let mut cur_data = Vec::<u8>::with_capacity(target_max_size as usize);
+    let mut next_data = Vec::<u8>::with_capacity(target_max_size as usize);
+    let mut out_data = Vec::<u8>::with_capacity(target_max_size as usize);
+
+    let mut dest_shards = PyList::empty(py);
+    let mut cur_si = MDBShardInfo::default();
+
+    for data in shard_list {
+        next_data = data;
+        let mut cursor = Cursor::new(&next_data);
+        
+        // Use MDBShardInfo::load_from_reader to get header and footer metadata.
+        // This is required for shard_set_union.
+        let shard_info = MDBShardInfo::load_from_reader(&mut cursor)
+            .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Parse error: {e:?}")))?;
+
+        if cur_data.is_empty() {
+            // Starting from scratch with the first shard
+            swap(&mut cur_data, &mut next_data);
+            cur_si = shard_info;
+        } else if cur_data.len() + next_data.len() - (size_of::<MDBShardFileHeader>() + size_of::<MDBShardFileFooter>())
+            <= target_max_size as usize
+        {
+            // We have enough size capacity to merge this one in.
+            out_data.clear();
+            cur_si = shard_set_union(
+                &cur_si,
+                &mut Cursor::new(&cur_data),
+                &shard_info,
+                &mut Cursor::new(&next_data),
+                &mut out_data,
+            ).map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("Merge error: {e:?}")))?;
+
+            // Now swap out the destination data with the current data.
+            swap(&mut out_data, &mut cur_data);
+        } else {
+            // Current buffer is full or would be too large; "flush" it and start new.
+            dest_shards.append(PyBytes::new(py, &cur_data))?;
+
+            // Move the loaded data into the current buffer.
+            swap(&mut cur_data, &mut next_data);
+            cur_si = shard_info;
+        }
+    }
+
+    // If there is any left over at the end, flush that as well.
+    if !cur_data.is_empty() {
+        dest_shards.append(PyBytes::new(py, &cur_data))?;
+    }
+
+    Ok(dest_shards.into_py(py))
+}
+
 #[pymodule]
 fn xet_shard_parser(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(extract_shard_metadata, m)?)?;
+    m.add_function(wrap_pyfunction!(merge_shards, m)?)?;
     Ok(())
 }
